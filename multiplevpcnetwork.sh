d3f86aa003962151c5731c10aa348d03b82eb5e2
bgiri_gcloud <49786519+bgirigcloud@users.noreply.github.com>
2023-09-29 21:11:52 +0530

    Update multiplevpcnetwork.sh

--- multiplevpcnetwork.sh
+++ multiplevpcnetwork.sh
@@ -1,10 +1,10 @@
 export ZONE=put zone name 
 
 #NOTE:- Make sure region 2 must be different from zone 
-
+export REGION_1=put region name 
 export REGION_2=put region name 
 export PROJECT_ID=put your project name 
-gcloud auth login --no-launch-browser
+#gcloud auth login --no-launch-browser
 gcloud config set project $PROJECT_ID
 
 
