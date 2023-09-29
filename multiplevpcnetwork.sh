e03458ae6885dd7f9c1be320e7b6e673c9f9aed3
bgiri_gcloud <49786519+bgirigcloud@users.noreply.github.com>
2023-09-29 20:19:43 +0530

    Update multiplevpcnetwork.sh

--- multiplevpcnetwork.sh
+++ multiplevpcnetwork.sh
@@ -3,7 +3,9 @@ export ZONE=put zone name
 NOTE:- Make sure region 2 must be different from zone 
 
 export REGION_2=put region name 
-
+export PROJECT_ID=put your project name 
+gcloud auth login --no-launch-browser
+gcloud config set project $PROJECT_ID
 
 
 curl -LO https://github.com/bgirigcloud/cloudhero-network-skill/blob/main/multiple-vpc-network.sh
